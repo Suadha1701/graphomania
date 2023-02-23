756fc9e30f0af720088fdc0f00782a2588913c3c
Subham <banerjee.subham1411@gmail.com>
2023-02-23 22:42:52 +0530

    bug fixed

--- frontend/major/src/App.js
+++ frontend/major/src/App.js
@@ -1,112 +1,181 @@
-import logo from './logo.svg';
-import React, { Component } from 'react'
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import FormValidator from './components/FormValidator';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import FormValidator from "./components/FormValidator";
 class App extends Component {
   constructor() {
     super();
-    this.validator = new FormValidator([{
-      field: 'full_name',
-      method: 'isEmpty',
-      validWhen: false,
-      message: 'Enter full name.'
-    }, {
-      field: 'email',
-      method: 'isEmpty',
-      validWhen: false,
-      message: 'Enter your email address.'
-    }, {
-      field: 'email',
-      method: 'isEmail',
-      validWhen: true,
-      message: 'Enter valid email address.'
-    }, {
-      field: 'phone',
-      method: 'isEmpty',
-      validWhen: false,
-      message: 'Enter a phone number.'
-    }, {
-      field: 'phone',
-      method: 'matches',
-      args: [/^\(?\d\d\d\)? ?\d\d\d-?\d\d\d\d$/],
-      validWhen: true,
-      message: 'Enter valid phone number.'
-    }, {
-      field: 'password',
-      method: 'isEmpty',
-      validWhen: false,
-      message: 'Enter password.'
-    }, {
-      field: 'password_confirmation',
-      method: 'isEmpty',
-      validWhen: false,
-      message: 'Enter Password confirmation.'
-    }, {
-      field: 'password_confirmation',
-      method: this.passwordMatch, // notice that we are passing a custom function here
-      validWhen: true,
-      message: 'Password and password confirmation do not match.'
-    }]);
+    this.validator = new FormValidator([
+      {
+        field: "full_name",
+        method: "isEmpty",
+        validWhen: false,
+        message: "Enter full name.",
+      },
+      {
+        field: "email",
+        method: "isEmpty",
+        validWhen: false,
+        message: "Enter your email address.",
+      },
+      {
+        field: "email",
+        method: "isEmail",
+        validWhen: true,
+        message: "Enter valid email address.",
+      },
+      {
+        field: "phone",
+        method: "isEmpty",
+        validWhen: false,
+        message: "Enter a phone number.",
+      },
+      {
+        field: "phone",
+        method: "matches",
+        args: [/^\(?\d\d\d\)? ?\d\d\d-?\d\d\d\d$/],
+        validWhen: true,
+        message: "Enter valid phone number.",
+      },
+      {
+        field: "password",
+        method: "isEmpty",
+        validWhen: false,
+        message: "Enter password.",
+      },
+      {
+        field: "password_confirmation",
+        method: "isEmpty",
+        validWhen: false,
+        message: "Enter Password confirmation.",
+      },
+      {
+        field: "password_confirmation",
+        method: this.passwordMatch, // notice that we are passing a custom function here
+        validWhen: true,
+        message: "Password and password confirmation do not match.",
+      },
+    ]);
     this.state = {
-      full_name: '',
-      email: '',
-      phone: '',
-      password: '',
-      password_confirmation: '',
+      full_name: "",
+      email: "",
+      phone: "",
+      password: "",
+      password_confirmation: "",
       validation: this.validator.valid(),
-    }
+    };
     this.submitted = false;
   }
-  passwordMatch = (confirmation, state) => (state.password === confirmation)
-  handleInputChange = event => {
+  passwordMatch = (confirmation, state) => state.password === confirmation;
+  handleInputChange = (event) => {
     event.preventDefault();
     this.setState({
       [event.target.name]: event.target.value,
     });
-  }
-  handleFormSubmit = event => {
+  };
+  handleFormSubmit = (event) => {
     event.preventDefault();
     const validation = this.validator.validate(this.state);
     this.setState({
-      validation
+      validation,
     });
     this.submitted = true;
     if (validation.isValid) {
       //reaches here if form validates successfully...
     }
-  }
+  };
   render() {
-    let validation = this.submitted ? this.validator.validate(this.state) : this.state.validation
+    let validation = this.submitted
+      ? this.validator.validate(this.state)
+      : this.state.validation;
     return (
       <div className="container ">
         <div className="row justify-content-center align-items-center min-vh-100">
           <div className="col-md-4 col-md-offset-4">
             <form className="registrationForm">
-              <h2 className='mb-5 text-center'>REGISTRATION</h2>
-              <div className={validation.email.isInvalid && 'has-error'}>
+              <h2 className="mb-5 text-center">REGISTRATION</h2>
+              <div className={validation.email.isInvalid && "has-error"}>
                 <label htmlFor="full_name">Full Name</label>
-                <input type="string" className="form-control" name="full_name" placeholder="Full Name" onChange={this.handleInputChange} /> <span className="help-block">{validation.full_name.message}</span> </div>
-              <div className={validation.email.isInvalid && 'has-error'}>
+                <input
+                  type="string"
+                  className="form-control"
+                  name="full_name"
+                  placeholder="Full Name"
+                  onChange={this.handleInputChange}
+                />{" "}
+                <span className="help-block">
+                  {validation.full_name.message}
+                </span>{" "}
+              </div>
+              <div className={validation.email.isInvalid && "has-error"}>
                 <label htmlFor="email">Email address</label>
-                <input type="email" className="form-control" name="email" placeholder="Email address" onChange={this.handleInputChange} /> <span className="help-block">{validation.email.message}</span> </div>
-              <div className={validation.phone.isInvalid && 'has-error'}>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email address"
+                  onChange={this.handleInputChange}
+                />{" "}
+                <span className="help-block">{validation.email.message}</span>{" "}
+              </div>
+              <div className={validation.phone.isInvalid && "has-error"}>
                 <label htmlFor="phone">Phone(enter only 10 digit number)</label>
-                <input type="phone" className="form-control" name="phone" placeholder="Phone Number" onChange={this.handleInputChange} /> <span className="help-block">{validation.phone.message}</span> </div>
-              <div className={validation.password.isInvalid && 'has-error'}>
+                <input
+                  type="phone"
+                  className="form-control"
+                  name="phone"
+                  placeholder="Phone Number"
+                  onChange={this.handleInputChange}
+                />{" "}
+                <span className="help-block">{validation.phone.message}</span>{" "}
+              </div>
+              <div className={validation.password.isInvalid && "has-error"}>
                 <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" placeholder="Password" name="password" onChange={this.handleInputChange} /> <span className="help-block">{validation.password.message}</span> </div>
-              <div className={validation.password_confirmation.isInvalid && 'has-error'}>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleInputChange}
+                />{" "}
+                <span className="help-block">
+                  {validation.password.message}
+                </span>{" "}
+              </div>
+              <div
+                className={
+                  validation.password_confirmation.isInvalid && "has-error"
+                }
+              >
                 <label htmlFor="password_confirmation">Confirm Password</label>
-                <input type="password" className="form-control" placeholder="Confirm Password" name="password_confirmation" onChange={this.handleInputChange} /> <span className="help-block">{validation.password_confirmation.message}</span> </div>
-              <div classname="d-flex justify-content-center">
-                <button onClick={this.handleFormSubmit} className="btn btn-primary mt-3 text-center"> Register </button>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Confirm Password"
+                  name="password_confirmation"
+                  onChange={this.handleInputChange}
+                />{" "}
+                <span className="help-block">
+                  {validation.password_confirmation.message}
+                </span>{" "}
+              </div>
+              <div
+                className="d-flex justify-content-center"
+              >
+                <button
+                  onClick={this.handleFormSubmit}
+                  className="btn btn-primary mt-3 text-center"
+                >
+                  {" "}
+                  Register{" "}
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
